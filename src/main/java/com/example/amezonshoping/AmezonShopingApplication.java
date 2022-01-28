ddb655fddc178e3b5e8a50420b7e6f28f2a7c66c
Dev Darshana <dinukaekanayake92@gmail.com>
2022-01-29 04:26:38 +0530

    first commit

---
+++ src/main/java/com/example/amezonshoping/AmezonShopingApplication.java
@@ -0,0 +1,13 @@
+package com.example.amezonshoping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmezonShopingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmezonShopingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/amezonshoping/AmezonShopingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amezonshoping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmezonShopingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
